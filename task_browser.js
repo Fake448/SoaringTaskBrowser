3331a4608c2b9bac042cdb757bc7af57373d0d30
guy.larochelle <guy@siglr.com>
2024-10-29 12:52:48 -0400

    Fixed the task count input box to be set with correct initial task fetched.

--- task_browser.js
+++ task_browser.js
@@ -92,7 +92,7 @@ class TaskBrowser {
 
         sliderContainer.innerHTML = `
             <input type="range" id="taskCountSlider" min="1" max="${tb.tbm.totalTasksInDB}" value="${tb.tbm.taskCount}" style="flex: 1; margin-right: 10px;">
-            <input type="number" id="taskCountInput" min="1" max="${tb.tbm.totalTasksInDB}" value="${tb.tbm.totalTasksInDB}" style="width: 60px; margin-right: 5px; text-align: right;">
+            <input type="number" id="taskCountInput" min="1" max="${tb.tbm.totalTasksInDB}" value="${tb.tbm.taskCount}" style="width: 60px; margin-right: 5px; text-align: right;">
         `;
         container.appendChild(sliderContainer);
 
