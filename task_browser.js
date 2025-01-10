e21fa1befb91792413415ed05f3d9ef4db4ac505
guy.larochelle <guy@siglr.com>
2025-01-09 20:20:56 -0500

    Changed the alert message after downloading.

--- task_browser.js
+++ task_browser.js
@@ -1351,7 +1351,7 @@ class TaskBrowser {
             .then(() => {
                 console.log("Local server call successful");
                 // If successful, we do NOT download the file from the server 
-                alert("Download successful! The DPHX file is now open in your local application.");
+                alert("Download successful! The DPHX file should now be opened in your local application.");
             })
             .catch(err => {
                 // Local call failed, so we do a normal file download as fallback
