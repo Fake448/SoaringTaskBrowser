a9b8215883a901ccac94363de1bff4a194ba91e6
guy.larochelle <guy@siglr.com>
2025-02-13 18:00:06 -0500

    Revert "Fixed panels not always expending when coming from URL"
    This reverts commit e2ebd2ec70ca895475f8511f2c7f24041870454b.

--- map2.js
+++ map2.js
@@ -474,7 +474,6 @@ class TaskBrowserMap {
     // Selecting a task from the "task" parameter in the URL string
     selectTaskFromURL(entrySeqID) {
         let tbm = this;
-        tbm.tb.fromURL = true;
         const entrySeqIDNbr = Number(entrySeqID);
         console.log("selectTaskFromURL()", entrySeqIDNbr);
 
@@ -485,6 +484,7 @@ class TaskBrowserMap {
         tbm.tb.getTaskDetails(entrySeqIDNbr, true).then(isAvailable => {
             if (isAvailable) {
                 // 3. Call the selectTaskCommon to perform the common actions
+                tbm.tb.fromURL = true;
                 tbm.selectTaskCommon(entrySeqIDNbr, true);
             } else {
                 console.log(`Task ${entrySeqIDNbr} is unavailable or could not be retrieved.`);
