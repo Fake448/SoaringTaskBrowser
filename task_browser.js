daea0192893f2a562ba4437df9ffe5515750b2eb
guy.larochelle <guy@siglr.com>
2025-02-24 10:51:16 -0500

    Revert "Trying by passing the task in parameter."
    This reverts commit 35c55550c58be725e6acdbe6a7a458afd4e2dc84.

--- task_browser.js
+++ task_browser.js
@@ -842,7 +842,7 @@ class TaskBrowser {
                 </a>
             </p>
             <p>
-                <a href="#" onclick="TB.downloadWPRFile(TB.currentTask)">
+                <a href="#" onclick="TB.downloadWPRFile()">
                     <img src="images/WPRFile.png" alt="WPR File" class="file-icon">
                     Weather file (WPR): ${tb.getFileNameFromPath(tb.currentTask.WPRFilename)}
                 </a>
