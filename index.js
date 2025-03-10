446994838ebf817e36eb2e2776232d8693b8523e
guy.larochelle <guy@siglr.com>
2025-03-10 16:49:51 -0400

    Adjusted Diamonds for Summer Time.

--- index.js
+++ index.js
@@ -279,8 +279,8 @@ function displayEventsStaticPortion() {
             <h3>Tuesday</h3>
             🕤 08:30 UTC: Ausglide Tuesday <em>(Daylight saving time)</em> - <a href="discord://discord.com/channels/876123356385149009/1066655140733517844">Event Channel</a></br>
             🕖 19:15 UTC: UKVGA Tuesday <em>(Normal time)</em> - <a href="discord://discord.com/channels/325227457445625856/1166042887084048515">Event Channel</a></br>
+            🕧 23:30 UTC: Diamonds Tuesday <em>(Daylight saving time)</em> - <a href="discord://discord.com/channels/793376245915189268/1097353400015921252">Event Channel</a></br>
             <h3>Wednesday</h3>
-            🕧 00:30 UTC: Diamonds Tuesday <em>(Normal time)</em> - <a href="discord://discord.com/channels/793376245915189268/1097353400015921252">Event Channel</a></br>
             🕡 18:45 UTC: SSC Wednesday <em>(Normal time)</em> - <a href="discord://discord.com/channels/876123356385149009/1128345453063327835">Event Channel</a></br>
             <h3>Thursday</h3>
             🕖 19:15 UTC: UKVGA Thursday <em>(Normal time)</em> - <a href="discord://discord.com/channels/325227457445625856/1166042920869175357">Event Channel</a></br>
