596ca8a8321e6f282b81222a1d20a81c463315d2
guy.larochelle <guy@siglr.com>
2025-03-29 22:34:20 -0400

    Removed parenthesis

--- index.js
+++ index.js
@@ -83,17 +83,17 @@ function loadTabContent(tabId) {
                 </div>
                 <p>Currently, you can use the tabs above to access the available features that are ready:</p>
                 <ul class="all-links">
-                    <li><a href="#" onclick="TB.switchTab('eventsTab')">(📆) Discover group flight events happening soon</a></li>
-                    <li><a href="#" onclick="TB.switchTab('mapTab')">(🌐) Explore tasks on the world map</a></li>
-                    <li><a href="#" onclick="TB.switchTab('toolsTab')">(🛠️) View a list of useful tools and other resources for soaring in MSFS</a></li>
-                    <li><a href="#" onclick="TB.switchTab('settingsTab')">(⚙️) Adjust your display settings here</a></li>
+                    <li><a href="#" onclick="TB.switchTab('eventsTab')">📆 Discover group flight events happening soon</a></li>
+                    <li><a href="#" onclick="TB.switchTab('mapTab')">🌐 Explore tasks on the world map</a></li>
+                    <li><a href="#" onclick="TB.switchTab('toolsTab')">🛠️ View a list of useful tools and other resources for soaring in MSFS</a></li>
+                    <li><a href="#" onclick="TB.switchTab('settingsTab')">⚙️ Adjust your display settings here</a></li>
                     <li>
                         <a href="#" onclick="TB.switchTab('accountTab')">
                             <img src="images/user_account_connected.png" alt="User Account" style="width: 20px; height: 20px; vertical-align: middle;">
                             Manage your account
                         </a>
                     </li>
-                    <li><a href="#" onclick="TB.switchTab('aboutTab')">( ℹ️ ) Learn a bit more about WeSimGlide.org</a></li>
+                    <li><a href="#" onclick="TB.switchTab('aboutTab')">ℹ️ Learn a bit more about WeSimGlide.org</a></li>
                 </ul>
                 <p>Tell us what else you would like to see on the home page!</p>
                 <a href="discord://discord.com/channels/1022705603489042472/1258192556202922107" target="_blank">
