f642592476a4126ff139c5117bfbf7493051a79d
guy.larochelle <guy@siglr.com>
2025-03-29 22:32:58 -0400

    Added user account line on the home page

--- index.js
+++ index.js
@@ -87,6 +87,12 @@ function loadTabContent(tabId) {
                     <li><a href="#" onclick="TB.switchTab('mapTab')">(🌐) Explore tasks on the world map</a></li>
                     <li><a href="#" onclick="TB.switchTab('toolsTab')">(🛠️) View a list of useful tools and other resources for soaring in MSFS</a></li>
                     <li><a href="#" onclick="TB.switchTab('settingsTab')">(⚙️) Adjust your display settings here</a></li>
+                    <li>
+                        <a href="#" onclick="TB.switchTab('accountTab')">
+                            <img src="images/user_account_connected.png" alt="User Account" style="width: 20px; height: 20px; vertical-align: middle;">
+                            Manage your account
+                        </a>
+                    </li>
                     <li><a href="#" onclick="TB.switchTab('aboutTab')">( ℹ️ ) Learn a bit more about WeSimGlide.org</a></li>
                 </ul>
                 <p>Tell us what else you would like to see on the home page!</p>
