f80616f935d13988b44991e94bd6605c2dbc3f06
guy.larochelle <guy@siglr.com>
2025-04-03 10:01:12 -0400

    "Uploaded on"

--- index.js
+++ index.js
@@ -314,7 +314,7 @@ function refreshIGCSubmissionsContent() {
                 <table id="userIGCRecordsTable" class="display igcRecordsTable" style="width: 100%;">
                     <thead>
                         <tr>
-                            <th>Upload</th>
+                            <th>Uploaded on</th>
                             <th>Task</th>
                             <th>Pilot</th>
                             <th>Glider</th>
