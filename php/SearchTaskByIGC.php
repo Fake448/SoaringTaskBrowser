60435cc54bed5f769cbc721533b8c3ab3f83a6a4
guy.larochelle <guy@siglr.com>
2025-04-15 16:16:31 -0400

    Debugging

--- php/SearchTaskByIGC.php
+++ php/SearchTaskByIGC.php
@@ -228,7 +228,7 @@ try {
 
             // --- BEGIN: Parse Browserless Response to Extract IGC Results ---
             logMessage("Parse Browserless Response to Extract IGC Results");
-            logmessage($browserlessResult);
+            logMessage("Browserless Result Array: " . print_r($browserlessResult, true));
             if (isset($browserlessResult['data']['tracklogsHTML']['html'])) {
                 $htmlContent = $browserlessResult['data']['tracklogsHTML']['html'];
                 $dom = new DOMDocument();
