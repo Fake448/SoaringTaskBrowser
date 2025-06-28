56a62b6a70733409bda0a5ae2d5647b02a5eb515
guy.larochelle <guy@siglr.com>
2025-06-27 23:47:00 -0400

    Reverting to previous version to review structure.

--- js/index.new.js
+++ js/index.new.js
@@ -1,15 +1,11 @@
 ﻿// js/index.new.js
 import Banner from './banner.js';
-import Splitter from './splitter.js';
-//import TabManager from './tabs/manager.js';
 
 // Core singletons
 const TB = new TaskBrowser();
 const igcUpload = new IGCUpload(TB);
 
 Banner.init();                 // pull in your banner.json marquee
-Splitter.init({ TB });         // wire up the resizer drag logic
-//TabManager.init({ TB, igcUpload });
 
 if (typeof TB.resizeMap === 'function') TB.resizeMap();
 
